022bcb2c74a6f52f182352f7a77634cb0cd2ce2b
zanibethel <zanibethel@gmail.com>
2026-07-11 11:08:05 -0500

    Create admin-dashboard-content.tsx
    Create admin dashboard content

---
+++ src/components/dashboards/admin/admin-dashboard-content.tsx
@@ -0,0 +1,9 @@
+import AdminOverviewSection from './sections/admin-overview-section'
+
+// =============================================================================
+// Component
+// =============================================================================
+
+export default function AdminDashboardContent() {
+  return <AdminOverviewSection />
+}
