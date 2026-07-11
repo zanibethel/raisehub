c7fc1c4a5330edd9e8c48c07bf0e55a3c2cdae9d
zanibethel <zanibethel@gmail.com>
2026-07-11 11:10:29 -0500

    Create admin-dashboard.tsx
    Extract admin dashboard

---
+++ src/components/dashboards/admin/admin-dashboard.tsx
@@ -0,0 +1,9 @@
+import AdminDashboardContent from './admin-dashboard-content'
+
+// =============================================================================
+// Admin dashboard
+// =============================================================================
+
+export default function AdminDashboard() {
+  return <AdminDashboardContent />
+}
