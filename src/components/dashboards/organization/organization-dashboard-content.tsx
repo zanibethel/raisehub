725251888f7fa01acbafa986b339bf9dfa4fddd0
zanibethel <zanibethel@gmail.com>
2026-07-11 10:52:03 -0500

    Update organization-dashboard-content.tsx
    Pass organization campaign metrics through content

--- src/components/dashboards/organization/organization-dashboard-content.tsx
+++ src/components/dashboards/organization/organization-dashboard-content.tsx
@@ -61,6 +61,7 @@ export default function OrganizationDashboardContent(
 
       <OrganizationCampaignsSection
         campaigns={props.campaigns}
+        metricsByCampaign={props.metricsByCampaign}
       />
     </div>
   )
